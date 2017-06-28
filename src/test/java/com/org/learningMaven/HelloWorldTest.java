113bba326f37213b6e4667666ed976c89a5014b6
r.singh <r.singh@CCPT-DESKTOP13.agilecockpit.local>
2017-06-28 15:55:18 +0530

    First Push

---
+++ src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.org.learningMaven;
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+	
+	@Test
+	public void Login() throws InterruptedException
+	{
+
+		System.out.println("Hello Test Baby");
+	}
+
+}
