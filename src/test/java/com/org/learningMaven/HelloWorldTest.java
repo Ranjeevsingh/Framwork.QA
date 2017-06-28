046d179aa2d4181718a0ae20c930e251abb5def2
r.singh <r.singh@CCPT-DESKTOP13.agilecockpit.local>
2017-06-28 19:35:21 +0530

    1122

--- src/test/java/com/org/learningMaven/HelloWorldTest.java
+++ src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -7,7 +7,7 @@ public class HelloWorldTest {
 	public void Login() throws InterruptedException
 	{
 
-		System.out.println("Hello Test Baby11");
+		System.out.println("Hello Test Baby1122");
 		
 	}
 
