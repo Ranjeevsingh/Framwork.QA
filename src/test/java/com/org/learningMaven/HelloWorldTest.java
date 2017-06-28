9ee391eecf5589e48dc9008c080759af79a06bb2
r.singh <r.singh@CCPT-DESKTOP13.agilecockpit.local>
2017-06-28 19:22:10 +0530

    another branch

--- src/test/java/com/org/learningMaven/HelloWorldTest.java
+++ src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -7,7 +7,7 @@ public class HelloWorldTest {
 	public void Login() throws InterruptedException
 	{
 
-		System.out.println("Hello Test Baby123");
+		System.out.println("Hello Test Baby1234");
 		
 	}
 
