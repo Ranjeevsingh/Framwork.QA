91e57fc9be221d5f36227f71ef33e8287e409329
r.singh <r.singh@CCPT-DESKTOP13.agilecockpit.local>
2017-07-03 16:21:58 +0530

    framworkChange

---
+++ src/test/java/com/org/Functional/Tests/Login.java
@@ -0,0 +1,13 @@
+package com.org.Functional.Tests;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	@Test
+	public void LoginToApplication() {
+		System.out.println("Actual Tests");
+	
+	}
+
+}
