42d93680303b8ccba055aa152719cdf1fc919686
r.singh <r.singh@CCPT-DESKTOP13.agilecockpit.local>
2017-07-13 13:46:01 +0530

    check cucumber

--- src/test/java/stepDefinitions/AppLandingPageStepDefinition.java
+++ src/test/java/stepDefinitions/AppLandingPageStepDefinition.java
@@ -1,6 +1,5 @@
 package stepDefinitions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
@@ -8,15 +7,15 @@ public class AppLandingPageStepDefinition {
 
 	
 	@Given("^I navigate to app landing page$")
-	public void i_navigate_to_app_landing_page() throws Throwable {
+	public void i_navigate_to_app_landing_page(){
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    System.out.println("SD1");
 	}
 
 	@Then("^I validate the user is on app landing page$")
 	public void i_validate_the_user_is_on_app_landing_page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    System.out.println("SD2");
 	}
 
 }
